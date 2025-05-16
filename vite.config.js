f4c8d2c31e8cd9a2c0a60b7bee865eedef8213ed
Shoaibali12 <sind98449@gmail.com>
2025-05-16 16:27:49 +0500

    modify packge.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/noon-academic-support/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/noon-academic-support/",
+  base: process.env.DEPLOY_ENV === "GH_PAGES" ? "/noon-academic-support/" : "/",
   plugins: [react()],
 });
